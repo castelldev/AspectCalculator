29d2c2d52e30def4ed3f6b726d974808517d6848
Franklin Castellanos <bloofrank55@gmail.com>
2018-11-28 13:56:40 -0500

    Code Cleanup
    Modified variable names for better code readability.

--- src/com/castell/Main.java
+++ src/com/castell/Main.java
@@ -22,14 +22,14 @@ public class Main {
         public void setHeight(double w){ this.height=w;}
 
         public double calcNewWidth(double newHeight){
-            newHeight*= this.ratio;
-            System.out.println("New Width:" + newHeight);
-            return newHeight;
+            double newWidth = newHeight*= this.ratio;
+            System.out.println("New Width:" + newWidth);
+            return newWidth;
         }
         public double calcNewHeight(double newWidth){
-            newWidth /= this.ratio;
-            System.out.println("New Height:" + newWidth);
-            return newWidth;
+            double newHeight = newWidth /= this.ratio;
+            System.out.println("New Height:" + newHeight);
+            return newHeight;
         }
         @Override
         public String toString() {
@@ -39,7 +39,7 @@ public class Main {
     }
     public static void main(String[] args) {
         Ratio ratio = new Ratio(640,480);
-        //ratio.calcNewHeight(300);
-        ratio.calcNewWidth(500);
+        //ratio.calcNewHeight(500);
+        //ratio.calcNewWidth(500);
     }
 }
